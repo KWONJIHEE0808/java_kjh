79f5d8c2f52c4369200d6c6ccebb697148c2eb20
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-25 13:57:21 +0900

    자바 1일차 : Hello world 출력 예제

---
+++ java/src/day1/Hello.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Hello {
+	// 콘솔 응용 프로그램이 실행되기 위해서 main(메소드)가 필요
+	/* 여러줄 주석
+	 * 주석1
+	 * 주석2
+	 */
+	// 한줄 주석은 주석기호부터 라인끝까지
+	/* println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함
+	 * print(문자열) : 문자열을 콘솔에 출력
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!!"); 
+		System.out.println("Hello World!!"); 
+		System.out.print("Hello World!!"); 
+	}
+
+}
