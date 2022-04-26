3ea4c6ea6ad1e7fa4d69cdf273c48353667486de
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-26 12:32:03 +0900

    자바 2일차 : 비교 연산자 예제

---
+++ java/src/day2/Ex5_Comparison.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class Ex5_Comparison {
+
+	public static void main(String[] args) {
+		/* >=을 반대로 쓰면 안됨 (ex. =>, =<)
+		 * ==을 = 과 혼동하지말것
+		 * 결과값은 true or false
+		 */
+		int score = 95;
+		System.out.println(score >= 90); //true
+		System.out.println(score <= 95); //true
+		System.out.println(score == 95); //true
+		System.out.println(score != 95); //false
+	
+		
+
+	}
+
+}
