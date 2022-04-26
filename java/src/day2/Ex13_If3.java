89317bc0513a21eb1b3dba4ec1f7254a7197d826
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-26 17:01:58 +0900

    자바 2일차 : else ir문 예제

---
+++ java/src/day2/Ex13_If3.java
@@ -0,0 +1,42 @@
+package day2;
+
+public class Ex13_If3 {
+
+	public static void main(String[] args) {
+		/* num가 양수이면 양수라고 출력하고,
+		 * num가 음수이면 음수라고 출력하고,
+		 * num가 0이면 0이라고 출력하는 코드를 if문을 이용하여 작성하세요.
+		 */
+		
+		int num = 5;
+
+		if(num == 0) {
+			System.out.println(num + "는 0");
+	    }
+		
+		else if(num > 0) {
+			System.out.println(num + "는 양수");
+		}
+		
+		else {
+			System.out.println(num + "는 음수");
+		}
+		
+		
+
+		if(num == 0) {
+			System.out.println(num + "0");
+	    }
+		
+		else if(num > 0) {
+			System.out.println(num + "는 양수");
+		}
+		
+		else if(num < 0) {
+			System.out.println(num + "는 음수");
+		}
+
+
+	}
+
+}
