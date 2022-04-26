d005b1b9fbab30306246ad5037d781e6065062cb
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-26 17:02:26 +0900

    자바 2일차 : if문 연습문제 - 달력예제

---
+++ java/src/day2/Ex15_Test3.java
@@ -0,0 +1,26 @@
+package day2;
+
+public class Ex15_Test3 {
+
+	public static void main(String[] args) {
+		/* month가 주어질 때 해당 달의 마지막 일을 출력하는 코드를 if문으로 작성하세요.
+		 * 31 : 1 3 5 7 8 10 12
+		 * 30 : 4 6 9 11
+		 * 28 : 2
+		 * 그 외의 달은 잘못된 월입니다라고 출력
+		 */
+		
+		int month = 5;
+		if(month < 1 || month > 12) {
+			System.out.println(month + "월은 잘못된 월입니다");
+		}else if(month == 2) {
+			System.out.println(month + "월의 마지막 일은 28일 입니다");
+		}else if(month == 4 || month == 6 || month == 9 || month == 11) {
+			System.out.println(month + "월의 마지막 일은 30일 입니다");
+		}else {
+			System.out.println(month + "월의 마지막 일은 31일 입니다");
+		}
+
+	}
+
+}
