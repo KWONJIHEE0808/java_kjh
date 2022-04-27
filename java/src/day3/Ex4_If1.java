4bfe699022fa43541610628a08f1a875f5438809
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-27 13:59:27 +0900

    자바 3일차 : 산술 연산자 입력 예제

---
+++ java/src/day3/Ex4_If1.java
@@ -0,0 +1,36 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Ex4_If1 {
+
+	public static void main(String[] args) {
+		/* 두 정수와 산술 연산자를 입력받아 산술 연산 결과를 출력하는 코드를 작성하세요
+		 * 
+		 */
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("두 정수와 산술 연산자를 입력하세요 : ");
+		int num1 = scan.nextInt();
+		char ch = scan.next().charAt(0);
+		int num2 = scan.nextInt();
+		
+		if(ch == '+'){
+			System.out.println(""+num1 + ch + num2 + "=" +(num1 + num2));
+		}else if(ch == '-') {
+			System.out.println(""+num1 + ch + num2 + "=" +(num1 - num2));
+		}else if(ch == '*') {
+			System.out.println(""+num1 + ch + num2 + "=" +(num1 * num2));
+		}else if(ch == '/') {
+			System.out.println(""+num1 + ch + num2 + "=" +(num1 / (double)num2));
+		}else if(ch == '%') {
+			System.out.println(""+num1 + ch + num2 + "=" +(num1 % num2));
+		}else {
+			System.out.println("잘못된 산술 연산자입니다");
+		}
+	    
+		scan.close();
+			
+	}
+	
+}
