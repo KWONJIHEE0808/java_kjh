61477468c7a0c7edc29f19cf0b6cc0f68e5719c9
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-28 17:00:22 +0900

    자바 4일차 : continue 예제

--- java/src/day3/Ex15_For6_GCD.java
+++ java/src/day3/Ex15_For6_GCD.java
@@ -13,15 +13,24 @@ public class Ex15_For6_GCD {
 		
 		/* 같은 타입의 변수를 여러개 선언하는 경우 ,를 통해 한줄로 선언할 수 있다(ex. case1, case2, case3;)
 		 */
-		
-		int num1=12, num2=18, i, gcd=1; //두 정수의 최대 공약수는 최소1이기 때문에 1로 초기화
-		
-		for(i=1;i<=num1;i++){
+		int num1 = 12, num2= 18, gcd=1;
+		for(int i=1; i <= num1 ; i++ ) {
 			if(num1 % i == 0 && num2 % i == 0) {
-				gcd = i; //i = gcd (X)
+				gcd = i;
 			}
 		}
-		System.out.println(num1+"와 "+num2+"의 최대 공약수는" + " = " + gcd);
-				
+		System.out.println(num1 + "과 " + num2 + "의 최대 공약수 : " + gcd);
+		/* 같은 타입의 변수를 여러개 선언하는 경우 ,를 통해 한줄로 선언할 수 있다
+		 * 타입 변수명1;
+		 * 타입 변수명2;
+		 * 타입 변수명3, 변수명4;
+		 * */
+		/* 변수의 사용 범위
+		 * i는 반복문 밖에 선언됐기 때문에 반복문 이후에도 사용 가능
+		 * j는 반복문 안에 선언됐기 때문에 반복문에서만 사용 가능 
+		 * */
+		int i;
+		for(i = 1; i<=5; i++);
+		for(int j = 1; j<=5; j++);	
 	}
 }
