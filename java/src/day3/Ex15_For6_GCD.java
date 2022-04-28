c8ba3318a07bd3d910c267da1ce5011d85cc0cc9
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-28 10:31:30 +0900

    자바 3일차 : for문 예제 - 최대 공약수 예제

---
+++ java/src/day3/Ex15_For6_GCD.java
@@ -0,0 +1,27 @@
+package day3;
+
+public class Ex15_For6_GCD {
+
+	public static void main(String[] args) {
+		/* 두 정수의 최대 공약수를 구하는 코드를 작성하세요.
+		 * 공약수 : 공통으로 있는 약수
+		 * 최대 공약수 : 공약수 중 가장 큰 수
+		 * ex 12 : 1 2 3 4 6 12
+		 * ex 18 : 1 2 3 6 9 18
+		 * -> 최대 공약수 : 6 
+		 */
+		
+		/* 같은 타입의 변수를 여러개 선언하는 경우 ,를 통해 한줄로 선언할 수 있다(ex. case1, case2, case3;)
+		 */
+		
+		int num1=12, num2=18, i, gcd=1; //두 정수의 최대 공약수는 최소1이기 때문에 1로 초기화
+		
+		for(i=1;i<=num1;i++){
+			if(num1 % i == 0 && num2 % i == 0) {
+				gcd = i; //i = gcd (X)
+			}
+		}
+		System.out.println(num1+"와 "+num2+"의 최대 공약수는" + " = " + gcd);
+				
+	}
+}
