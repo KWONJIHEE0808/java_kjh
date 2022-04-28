1fea9fa20f6baf6df6ff501af29696c4fdaa8aaa
KWONJIHEE0808 <jeehee16@naver.com>
2022-04-28 16:59:57 +0900

    자바 4일차 : 무한루프 에제

---
+++ java/src/day4/Ex8_InfiniteLoop.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class Ex8_InfiniteLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i>0;i++) {
+			System.out.println(i);
+		}
+		System.out.println("안녕하세요");
+		/* 1부터 int의 최대 정수까지 반복함, 무한루프는 아님
+		 * 
+		 */
+
+	}
+
+}
