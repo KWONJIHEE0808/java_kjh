a46c23033376100bcad70760cbee44c4cb7f3aed
KWONJIHEE0808 <jeehee16@naver.com>
2022-08-02 16:02:12 +0900

    spring : 스프링데이터 전송

--- spring_kjh/src/main/java/kr/green/spring/HomeController.java
+++ spring_kjh/src/main/java/kr/green/spring/HomeController.java
@@ -1,16 +1,42 @@
 package kr.green.spring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value= {"/"})
-	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView home(ModelAndView mv, String hobby, Integer time){
 	    mv.setViewName("/main/home");
-	    mv.addObject("setHeader", "타일즈");
+	    mv.addObject("name", "홍길동");
+	    mv.addObject("age", "20");
+	    System.out.println("취미는 "+hobby+"이고 "+time+"시간씩 합니다.");
+	    return mv;
+	}	
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView homePost(ModelAndView mv, String hobby, Integer time){
+	    mv.setViewName("/main/home");
+	    System.out.println("취미는 "+hobby+"이고 "+time+"시간씩 합니다.");
+	    return mv;
+	}	
+	
+	@RequestMapping(value= "/hobby/{hobby1}/{time1}")
+	public ModelAndView hobby(ModelAndView mv, 
+			@PathVariable("hobby1") String hobby, 
+			@PathVariable("time1") Integer time){
+	    mv.setViewName("redirect:/");
+	    System.out.println("취미는 "+hobby+"이고 "+time+"시간씩 합니다.");
+	    return mv;
+	}	
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public ModelAndView loginGet(ModelAndView mv){
+	    mv.setViewName("/main/login");
 	    return mv;
 	}	
 	
--- spring_kjh/src/main/webapp/WEB-INF/views/main/home.jsp
+++ spring_kjh/src/main/webapp/WEB-INF/views/main/home.jsp
@@ -1,10 +1,46 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<div style="height: 800px">
-	<h1>
-		Hello world!  
-	</h1>
-	<P> ${setHeader}. </P>
-</div>
 
+
+<div class="container">
+	<h6>안녕하세요. ${name} 입니다. 제 나이는 ${age}살입니다.</h6>
+	<a href="/spring?hobby=운동&time=2">a태그를 이용한 데이터 전송</a>
+	<form action="/spring/" class="mt-3" method="post">
+		<div class="form=group">
+			<input type="text" class="form-control" name="hobby" placeholder="취미">
+		</div>
+		<div class="form=group">
+			<input type="text" class="form-control" name="time" placeholder="시간">
+		</div>
+		<button class="btn btn-outline-success col-12 mb-3">전송</button>
+	</form>
+	<a href="/spring/hobby/운동/3">경로 변수 연습링크</a>
+	<script type="text/javascript">
+		$(function(){
+			$('form').submit(function(){
+				let hobby = $('[name=hobby]').val()
+				if(hobby.length == 0){
+					alert('취미를 입력하세요')
+					$('[name=hobby]').focus();
+					return false;
+				}
+				
+				let time = $('[name=time]').val();
+				let timeRegex = /^[0-9]+$/
+				if(!timeRegex.test(time)){
+					alert('정수만 입력하세요');
+					$('[name=time]').focus();
+					return false;
+				}
+			})
+		})
+	
+	
+		$(function(){
+			let age = '${age}';
+			age = parseInt(age) + 10;
+			console.log(age)
+		})
+	</script>
+</div>
