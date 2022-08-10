25cf11fc10f146199a2d10ba9debe7aa0d2fc3e5
KWONJIHEE0808 <jeehee16@naver.com>
2022-08-10 17:10:12 +0900

    spring : 게시글 리스트

--- spring_kjh/src/main/java/kr/green/spring/service/MemberServiceImp.java
+++ spring_kjh/src/main/java/kr/green/spring/service/MemberServiceImp.java
@@ -8,61 +8,64 @@ import kr.green.spring.dao.MemberDAO;
 import kr.green.spring.vo.MemberVO;
 
 @Service
-public class MemberServiceImp implements MemberService{
-	
-	@Autowired
-	MemberDAO memberDao;
+public class MemberServiceImp implements MemberService {
 
 	@Autowired
+	MemberDAO memberDao;
+	
+	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
+	
 	@Override
 	public boolean signup(MemberVO member) {
-		if(member == null) {
+		//유효성 검사
+		if(member == null)
 			return false;
-		}if(member.getMe_id() == null || member.getMe_id().length() == 0) {
+		
+		if(member.getMe_id() == null || member.getMe_id().length() == 0)
 			return false;
-		}if(member.getMe_pw() == null || member.getMe_pw().length() == 0) {
+		
+		if(member.getMe_pw() == null || member.getMe_pw().length() == 0)
 			return false;
-		}if(member.getMe_email() == null || member.getMe_email().length() == 0) {
+		
+		if(member.getMe_email() == null || member.getMe_email().length() == 0)
 			return false;
-		}if((member.getMe_gender() != 'M' && member.getMe_gender() != 'F')) {
+		
+		if(member.getMe_gender() != 'M' && member.getMe_gender() != 'F')
 			return false;
-		}if(member.getMe_birth() == null) {
-				return false;
-		}		
+		
+		if(member.getMe_birth() == null)
+			return false;
 		
 		MemberVO dbMember = memberDao.selectMember(member.getMe_id());
+		//이미 가입된 아이디이면 
 		if(dbMember != null)
 			return false;
 		
-		//입혁한 비밀번호 암호와
+		//입력한 비밀번호를 암호화
 		String encodePw = passwordEncoder.encode(member.getMe_pw());
-		//암호화된 비밀번호를 회원 비번으로 설정
+		//암호화된 비밀번호를 회원 비번으로 설정 
 		member.setMe_pw(encodePw);
+		
 		memberDao.insertMember(member);
 		return true;
 	}
 
 	@Override
 	public MemberVO login(MemberVO member) {
-		if(member == null || member.getMe_id() == null) {
+		if(member == null || member.getMe_id() == null)
 			return null;
-		}
 		
-		//가입된 아이디가 아니면
 		MemberVO dbMember = memberDao.selectMember(member.getMe_id());
-		if(dbMember == null) {
+		//가입된 아이디가 아니면
+		if(dbMember == null)
 			return null;
-		}
 		
-		//아이디, 비번이 일치하지 않는 경우
+		//아이디, 비번이 일치하는 경우
 		//matches(암호화안된비번, 암호화된비번)
-		if(passwordEncoder.matches(member.getMe_pw(), dbMember.getMe_pw())){
+		if(passwordEncoder.matches(member.getMe_pw(), dbMember.getMe_pw()))
 			return dbMember;
-		}
-		
 		//아이디는 있지만 비번이 다른 경우
 		return null;
 	}
 }
-
