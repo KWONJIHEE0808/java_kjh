82cc944e4189cf3aeddce1dc72d8a938db22ad44
KWONJIHEE0808 <jeehee16@naver.com>
2022-08-18 17:10:45 +0900

    spring : 게시글 답글 순서 정렬

--- spring_kjh/src/main/java/kr/green/spring/vo/BoardVO.java
+++ spring_kjh/src/main/java/kr/green/spring/vo/BoardVO.java
@@ -19,12 +19,12 @@ public class BoardVO {
 	private int bd_depth;
 	private int bd_views;
 	private char bd_del;
+	private int bd_order;//같은 ori_num를 가진 게시글들의 순서
 	
 	public String getBd_reg_date_str() {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		return format.format(bd_reg_date);
 	}
-	
 	public String getBd_reg_date_time_str() {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		return format.format(bd_reg_date);
