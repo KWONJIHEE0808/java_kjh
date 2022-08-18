2beb76ee5cddb0e33b3088fe8b22b87b440acec0
KWONJIHEE0808 <jeehee16@naver.com>
2022-08-18 15:11:34 +0900

    spring : 답글 기능

--- spring_kjh/src/main/java/kr/green/spring/interceptor/MemberInterceptor.java
+++ spring_kjh/src/main/java/kr/green/spring/interceptor/MemberInterceptor.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class MemberInterceptor extends HandlerInterceptorAdapter{
+public class MemberInterceptor extends HandlerInterceptorAdapter {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, 
@@ -16,7 +16,7 @@ public class MemberInterceptor extends HandlerInterceptorAdapter{
 		HttpSession session = request.getSession();
 		Object user = session.getAttribute("user");
 		if(user == null) {
-			response.sendRedirect(request.getContextPath()+"/");
+			response.sendRedirect(request.getContextPath()+"/login");
 			return false;
 		}
 		return true;
